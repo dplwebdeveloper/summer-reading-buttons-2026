22c391847f94a95788f889499071d568c51f5e58
Emi Jackson <emij@umich.edu>
2026-05-27 17:55:37 -0400

    feat/update styling

--- app/components/ButtonsList.js
+++ app/components/ButtonsList.js
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { allButtons } from "../data/libraries";
-import styles from "./ButtonsList.module.css";
-import { useEffect, useState } from "react";
-import { getRandomInt, shuffle } from "../helpers";
-import Link from "next/link";
-import Refresh from "./icons/Refresh";
-import { useButtonsContext } from "./ButtonsProvider";
+import Image from 'next/image';
+import { allButtons } from '../data/libraries';
+import styles from './ButtonsList.module.css';
+import { useEffect, useState } from 'react';
+import { getRandomInt, shuffle } from '../helpers';
+import Link from 'next/link';
+import Refresh from './icons/Refresh';
+import { useButtonsContext } from './ButtonsProvider';
 
 export default function ButtonsList() {
   const [shuffleKey, setShuffleKey] = useState(0);
@@ -19,9 +19,8 @@ export default function ButtonsList() {
     <div className={styles.wrapper}>
       <div className={styles.intro}>
         <p>
-          Libraries (and library supporters) created and shared buttons with
-          each other to promote summer reading in Michigan. This year's summer
-          reading theme is <a href="/about#summer-reading">Unearth a Story</a>.
+          Libraries (and library supporters) created and shared buttons with each other to promote
+          summer reading in Michigan. This year's summer reading theme is <em>Unearth a Story</em>.
         </p>
       </div>
       <div className={styles.actions}>
@@ -41,12 +40,9 @@ export default function ButtonsList() {
       {shuffleKey > 0 && (
         <div className={styles.buttons} key={shuffleKey}>
           {buttons.map((b, i) => (
-            <Link
-              href={`/${b.libraryId}?btn=${b.index}`}
-              key={`${b.libraryId}_${b.index}`}
-            >
+            <Link href={`/${b.libraryId}?btn=${b.index}`} key={`${b.libraryId}_${b.index}`}>
               <Image
-                className={i < 20 ? styles[`delay${getRandomInt(0, 3)}`] : ""}
+                className={i < 20 ? styles[`delay${getRandomInt(0, 3)}`] : ''}
                 src={b?.image.src}
                 width={b?.image.width}
                 height={b?.image.height}
